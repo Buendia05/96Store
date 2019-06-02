8c3485c77fe912b69c070471c509a7c6183e8e34
Flamenco92 <oscarflamenco15@gmail.com>
2019-06-01 21:55:33 -0600

    v4 reportes

--- modulos/admin.php
+++ modulos/admin.php
@@ -35,50 +35,52 @@ if(isset($_SESSION['id_admins'])){ // si hay una sesion iniciada
 		<h1>Administracion</h1>
 		<br>
 		<div class="row">
-		<div class="col-xs-12 col-lg-2" style="float: center;">
+		<div class="col-xs-12 col-lg-3" style="float: center;">
 			<a href="?p=frm_admin" style="font-family: 'Orbitron','Arial'; color: #fff;">
-			<button class="btn btn-warning"><i class="fa fa-plus-circle"></i> Administrador</button></a>
+			<button class="btn btn-warning"><i class="fa fa-plus-circle"></i> Crear Administrador</button></a>
 		</div>
 
-		<div class="col-xs-12 col-lg-2" style="float: left;">
+		<div class="col-xs-12 col-lg-3" style="float: left;">
 			<a  href="?p=agregar_producto" style=" font-family: 'Orbitron','Arial'; color: #fff;">
-			<button class="btn btn-warning"><i class="fa fa-plus-circle"></i> Productos</button></a>
+			<button class="btn btn-warning"><i class="fa fa-plus-circle"></i> Agregar Productos</button></a>
 		</div>
 
-		<div class="col-xs-12 col-lg-2" style="float: center;">
+		<div class="col-xs-12 col-lg-3" style="float: center;">
 			<a href="?p=agregar_categoria" style="font-family: 'Orbitron','Arial'; color: #fff;">
-			<button class="btn btn-warning"><i class="fa fa-plus-circle"></i> Categoria</button></a>
+			<button class="btn btn-warning"><i class="fa fa-plus-circle"></i> Agregar Categoria</button></a>
 		</div>
 
-		<div class="col-xs-12 col-lg-2" style="float: center;">
+		<div class="col-xs-12 col-lg-3" style="float: center;">
 			<a href="?p=manejar_tracking" style="font-family: 'Orbitron','Arial'; color: #fff;">
-			<button class="btn btn-warning"><i class="fa fa-plus-circle"></i> Tracking</button></a>
+			<button class="btn btn-warning"><i class="fa fa-plus-circle"></i> Ver Estado</button></a>
 		</div>
 
-		<div class="col-xs-12 col-lg-2" style="float: center;">
-			<a href="?p=pagos" style="font-family: 'Orbitron','Arial'; color: #fff;">
-			<button class="btn btn-warning"><i class="fa fa-plus-circle"></i> Pagos</button></a>
-		</div>
-
+</div>
+<br>
+<div class="row">
+	<div class="col-xs-12 col-lg-3" style="float: center;">
+		<a href="?p=pagos" style="font-family: 'Orbitron','Arial'; color: #fff;">
+		<button class="btn btn-warning"><i class="fa fa-plus-circle"></i> Ver Pagos</button></a>
+	</div>
 </div>
 
 <hr>
 <h1>Reportes</h1>
 <br>
 <div class="row">
-	<div class="col-xs-12 col-lg-4" style="float: center;">
-		<a href="?p=pagos" style="font-family: 'Orbitron','Arial'; color: #fff;">
-		<button class="btn btn-warning"><i class="fa fa-plus-circle"></i> Reporte de Ventas</button></a>
+	<div class="col-xs-12 col-lg-3" style="float: center;">
+		<a href="recursos/reportesPDF/reporteProductos.php" target="_blank" class="btn btn-warning" style="font-family: 'Orbitron','Arial'; color: #fff;">
+		<i class="fa fa-plus-circle"></i> Reporte de Productos</a>
 	</div>
 
-	<div class="col-xs-12 col-lg-4" style="float: center;">
-		<a href="?p=pagos" style="font-family: 'Orbitron','Arial'; color: #fff;">
-		<button class="btn btn-warning"><i class="fa fa-plus-circle"></i> Reporte de Ventas</button></a>
+	<div class="col-xs-12 col-lg-3" style="float: center;">
+		<a href="recursos/reportesPDF/reporteVentas.php" target="_blank" class="btn btn-warning" style="font-family: 'Orbitron','Arial'; color: #fff;">
+		<i class="fa fa-plus-circle"></i> Reporte de Ventas</a>
 	</div>
 
-	<div class="col-xs-12 col-lg-4" style="float: center;">
-		<a href="?p=pagos" style="font-family: 'Orbitron','Arial'; color: #fff;">
-		<button class="btn btn-warning"><i class="fa fa-plus-circle"></i> Reporte de Clientes</button></a>
+	<div class="col-xs-12 col-lg-3" style="float: center;">
+		<a href="recursos/reportesPDF/reporteClientes.php" target="_blank" class="btn btn-warning" style="font-family: 'Orbitron','Arial'; color: #fff;">
+		<i class="fa fa-plus-circle"></i> Reporte de Clientes</a>
 	</div>
 </div>
 </div>
